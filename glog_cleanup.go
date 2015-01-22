518a8acb246e7ca76bb4620c5bee22c030f7da61
Michael Stapelberg <michael@stapelberg.de>
2015-01-22 09:36:49 +0100

    Implement old log file cleanup.
    See the new -log_total_bytes flag.

---
+++ glog_cleanup.go
@@ -0,0 +1,88 @@
+// Go support for leveled logs, analogous to https://code.google.com/p/google-glog/
+//
+// Copyright 2013 Google Inc. All Rights Reserved.
+// Copyright 2015 Michael Stapelberg. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// File cleanup for logs.
+
+package glog
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+)
+
+var logTotalBytes = flag.Int64("log_total_bytes",
+	-1,
+	"Restrict total size of log files created by this binary to this number of bytes")
+
+// cleanup deletes old log files created by this binary (not restricted to the
+// current pid), when their total size exceeds -log_cleanup_bytes
+func cleanup() {
+	if *logTotalBytes == -1 {
+		return
+	}
+
+	// Must match how logName() works.
+	prefix := fmt.Sprintf("%s.%s.%s.log.",
+		program,
+		host,
+		userName)
+
+	for _, dir := range logDirs {
+		logdir, err := os.Open(dir)
+		if err != nil {
+			continue
+		}
+		defer logdir.Close()
+		infos, err := logdir.Readdir(-1)
+		if err != nil {
+			continue
+		}
+
+		var total int64
+		sorted := make([]string, 0, len(infos))
+		sizes := make(map[string]int64)
+		for _, fi := range infos {
+			name := fi.Name()
+			if !strings.HasPrefix(name, prefix) {
+				continue
+			}
+			sorted = append(sorted, name)
+			sizes[name] = fi.Size()
+			total += fi.Size()
+		}
+		// Save the sort if there is nothing to clean up.
+		if total < *logTotalBytes {
+			continue
+		}
+		sort.Strings(sorted)
+
+		for total > *logTotalBytes && len(sorted) > 0 {
+			oldest := sorted[0]
+			name := filepath.Join(dir, sorted[0])
+			if err := os.Remove(name); err != nil {
+				// Print directly to stderr, as we cannot use log (might create a cycle).
+				fmt.Fprintf(os.Stderr, "Could not clean up old logfile %q: %v\n", name, err)
+			}
+			total -= sizes[oldest]
+			sorted = sorted[1:]
+		}
+	}
+}
